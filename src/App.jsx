495d8c1cfeb1e17766e79d60efb17c0ea889c63e
Yonatan Ariel <yonatanriel@gmail.com>
2024-10-24 02:44:44 +0300

    continue styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,25 @@ import { questions } from "./data";
 function App() {
   return (
     <div className="min-h-screen w-full flex justify-center ">
-      <ul className="w-9/12 bg-orange-600">
-        {questions.map((q) => (
-          <li key={q.question} className="bg-red-400 h-screen">
-            <h3>{q.question}</h3>
-            <Answers answers={q.answers} />
+      <img
+        // src="\light_purple.webp"
+        src="\light_purple2.jpg"
+        alt=""
+        className="fixed h-screen w-full -z-10"
+      />
+      <ul className="w-10/12 sm:w-8/12 lg:w-6/12 ">
+        {questions.map((q, i) => (
+          <li
+            key={q.question}
+            className=" h-screen flex flex-col justify-center "
+          >
+            <div className="rounded-md p-5  h-fit ">
+              <h3 className=" text-xl min-[400px]:text-2xl mb-8 text-strongPurple font-bold">
+                <span className="text-regularPurple">{`${i + 1}. `}</span>
+                {q.question}
+              </h3>
+              <Answers answers={q.answers} />
+            </div>
           </li>
         ))}
       </ul>
--- src/components/ui/answers.jsx
+++ src/components/ui/answers.jsx
@@ -4,11 +4,20 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 function Answers({ answers }) {
   return (
     <>
-      <RadioGroup>
+      <RadioGroup className="flex flex-col gap-5">
         {answers.map((answer) => (
-          <div key={answer}>
-            <RadioGroupItem value={answer} id={answer} />
-            <Label htmlFor={answer}>{answer}</Label>
+          <div
+            key={answer}
+            className="flex items-center gap-3 p-4 rounded-2xl bg-purple-50 bg-opacity-35 border hover:border-lightPurple hover:scale-[101%] duration-75 transition-transform border-regularPurple border-solid"
+          >
+            <RadioGroupItem
+              value={answer}
+              id={answer}
+              className="w-6 h-6 color-strongPurple"
+            />
+            <Label className="text-base  text-strongPurple" htmlFor={answer}>
+              {answer}
+            </Label>
           </div>
         ))}
       </RadioGroup>
