7fc3c33cfab1c33353d55ef1941e6006f9b2c3b7
Yonatan Ariel <yonatanriel@gmail.com>
2024-10-25 08:21:45 +0300

    adding gradient background animation to home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+  return (
+    <div className="w-full h-screen animate-backgroundColor bg-gradient-to-r from-veryLightPurple to-lightPurple"></div>
+  );
+}
+export default Home;
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,12 +1,13 @@
+import Home from "@/pages/home";
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const Quiz = lazy(() => import("@/pages/quiz"));
-// import Quiz from "@/pages/quiz";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route
         path="quiz"
         element={
